ae2e5278aeb2eaf3c22c738c87754ac5256cd275
ferdinand <ferdinand.vanwalree@gmail.com>
2020-12-19 23:05:12 +0000

    Experimenting structure of torrent file

--- src/main.cpp
+++ src/main.cpp
@@ -20,62 +20,40 @@ int main() {
     ss << fs.rdbuf();
     auto v = bencode::decode<bencode::bdata>(ss);
     auto bdict = v.value().get_bdict().value();
-    auto info = bdict.value().at(bencode::bstring("info"));
-    for (auto kvp : info->get_bdict()->value()) {
+    for (auto kvp : bdict.key_values()) {
+        bencode::bstring k = kvp.first;
+        cout << "Key: " << k << " " << kvp.second->display_type() << endl;
+    }
+
+    auto ann = bdict.at("announce");
+    cout << "announce: " << *ann << endl;
+    auto ann_list = bdict.at("announce-list");
+    // cout << *ann_list << endl;
+
+    auto comment = bdict.at("comment");
+    cout << "comment: " << *comment << endl;
+
+    auto created_by = bdict.at("created by");
+    cout << "created by: " << *created_by << endl;
+    auto creation_date = bdict.at("creation date");
+    cout << "creation date: " << *creation_date << endl;
+    auto encoding = bdict.at("encoding");
+    cout << "encoding: " << *encoding << endl;
+
+    auto info = bdict.at("info")->get_bdict().value();
+    for (auto kvp : info.key_values()) {
         bencode::bstring k = kvp.first;
         cout << k << " " << kvp.second->display_type() << endl;
     }
-    // for_each(bdict.value().begin(), bdict.value().end(), [](pair<bencode::bstring,shared_ptr<bencode::bdata>> kvp) { cout << kvp.first << endl; });
-    // cout << v.value() << endl;
+
+    auto name = info.at("name");
+    cout << "name: " << *name << endl;
+    auto piece_length = info.at("piece length");
+    cout << "piece length: " << *piece_length << endl;
+
+    auto files = info.at("files");
+    for (auto item : files->get_blist()->value()) {
+        bencode::bdict k = item->get_bdict().value();
+        cout << k << endl;
+    }
 };
-    // auto decodedData = bencoding::decode(file);
-    // std::string prettyString = bencoding::getPrettyRepr(decodedData);
-    // std::cout << prettyString;
-
-    // std::vector<std::string> keys;
-
-    // auto begin {value.begin()};
-    // auto end {value.end()};
-
-    // for (auto p {begin}; p != end; p++) { keys.push_back((*p).first);}
-
-    // std::ofstream outFile("examples/out.txt");
-    // std::ostream_iterator<std::string> output_iter(outFile,"\n");
-
-    // auto url = value["anounce"];
-
-
-    // // auto announceList = boost::get<std::vector<bencode::data>>(value["announce-list"]);
-    // // std::transform(announceList.begin(),announceList.end(),announceList.begin(),[](bencode::data dInner)
-    // //     {
-    // //         auto innerVec = boost::get<std::vector<bencode::data>>(dInner);
-    // //         auto innerVec2 = std::transform(innerVec.begin(),innerVec.end(),innerVec.begin(),[](bencode::data dVal) 
-    // //         {
-    // //             auto val = boost::get<std::string>(dVal);
-    // //             return val;
-    // //         });
-    // //     });
-
-    // // std::cout << announceList;
-
-    // std::cout << "oh";
-    // auto comment = value["comment"];
-    // std::cout << "test";
-    // std::cout << comment;
-    // std::cout << "test2";
-
-    // auto creationDate = value["created date"];
-    // std::cout << creationDate;
-
-    // auto createdBy = value["created by"];
-    // std::cout << createdBy;
-
-    // auto encoding = value["encoding"];
-    // std::cout << encoding;
-
-
-
-    // std::copy(keys.begin(), keys.end(), output_iter);
-//     return 0;
-
-// };
