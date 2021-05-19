f2ec4033cbed25cca9f551d8952f5b56fab2a51b
ferdinand <ferdinandvwalree@gmail.com>
2021-05-19 23:07:01 +0100

    Testing with sqlite_orm header library

--- include/persist/init_db.h
+++ include/persist/init_db.h
@@ -19,6 +19,14 @@ class DBHandle {
         int close();
 };
 
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+   int i;
+   for(i = 0; i<argc; i++) {
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
 
 template <>
 class DBHandle<DBType::SQLite3> {
@@ -29,10 +37,20 @@ class DBHandle<DBType::SQLite3> {
 
         int open(std::string filepath) {
             sqlite3 *t_handle;
-            sqlite3_open(filepath.c_str(), &t_handle);
+            char *zErrMsg = 0;
+            auto ret = sqlite3_open(filepath.c_str(), &t_handle);
 
             handle = std::shared_ptr<sqlite3>(t_handle,&sqlite3_close);
 
-            return 0;
+            auto sql = "CREATE TABLE COMPANY("  \
+                "ID INT PRIMARY KEY     NOT NULL," \
+                "NAME           TEXT    NOT NULL," \
+                "AGE            INT     NOT NULL," \
+                "ADDRESS        CHAR(50)," \
+                "SALARY         REAL );";
+
+            sqlite3_exec(handle.get(), sql, callback, 0, &zErrMsg);
+
+            return ret;
         }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,96 @@
 #include "network/p2p/BitTorrent.h"
 #include "common/logger.h"
 #include "persist/init_db.h"
+#include "sqlite_orm/sqlite_orm.h"
 
 using namespace boost::asio;
 
+struct TorrentF {
+    int id;
+    std::string name;
+    std::string meta_info_path;
+    std::string write_path;
+};
+
+struct PieceF {
+    int id;
+    int torrent_id;
+    int piece;
+};
+
+struct Announce {
+    int id;
+    int torrent_id;
+    time_t datetime;
+};
+
+struct AnnouncePeer {
+    int id;
+    int announce_id;
+    std::string ip;
+    int port;
+};
+
 int main() {
-    DBHandle<DBType::SQLite3> db;
+    using namespace sqlite_orm;
+    auto storage = make_storage("torrents.db",
+                                make_table("torrent",
+                                           make_column("id", &TorrentF::id, primary_key()),
+                                           make_column("name", &TorrentF::name),
+                                           make_column("meta_info_path", &TorrentF::meta_info_path),
+                                           make_column("write_path", &TorrentF::write_path)),
+                                make_table("torrent_piece",
+                                           make_column("id", &PieceF::id, primary_key()),
+                                           make_column("torrent_id", &PieceF::torrent_id),
+                                           make_column("piece", &PieceF::piece),
+                                           foreign_key(&PieceF::torrent_id).references(&TorrentF::id)),
+                                make_table("torrent_announce",
+                                          make_column("id",&Announce::id,primary_key()),
+                                          make_column("torrent_id",&Announce::torrent_id),
+                                          make_column("datetime",&Announce::datetime),
+                                          foreign_key(&Announce::torrent_id).references(&TorrentF::id)),
+                                make_table("announce_peer",
+                                         make_column("id",&AnnouncePeer::id,primary_key()),
+                                         make_column("announce_id",&AnnouncePeer::announce_id),
+                                         make_column("ip",&AnnouncePeer::ip),
+                                         make_column("port",&AnnouncePeer::port),
+                                         foreign_key(&AnnouncePeer::announce_id).references(&Announce::id)));
+    storage.sync_schema();
 
-    //get current time and set as seed for rand
-    srand ( time(NULL) );
+    auto torr = TorrentF{56,"name","meta","write"};
+    auto torr_id = storage.insert(torr);
+    std::cout << torr_id << std::endl;
 
-    //sets attributes such as timestamp, thread id
-    boost::log::add_common_attributes();
+    // //get current time and set as seed for rand
+    // srand ( time(NULL) );
 
-    //set log file as logging destination
-    boost::log::add_file_log(
-        boost::log::keywords::file_name = "log.txt",
-        //this second parameter is necessary to force the log file
-        //to be named 'log.txt'. It is a known bug in boost1.74
-        // https://github.com/boostorg/log/issues/104
-        boost::log::keywords::target_file_name = "log.txt",
-        boost::log::keywords::auto_flush = true,
-        boost::log::keywords::format = "[%ThreadID%][%TimeStamp%] %Message%"
-    );
+    // //sets attributes such as timestamp, thread id
+    // boost::log::add_common_attributes();
 
-    std::shared_ptr<std::mutex> mu = make_shared<std::mutex>();
-    std::unique_lock<std::mutex> lock(*mu.get());
+    // //set log file as logging destination
+    // boost::log::add_file_log(
+    //     boost::log::keywords::file_name = "log.txt",
+    //     //this second parameter is necessary to force the log file
+    //     //to be named 'log.txt'. It is a known bug in boost1.74
+    //     // https://github.com/boostorg/log/issues/104
+    //     boost::log::keywords::target_file_name = "log.txt",
+    //     boost::log::keywords::auto_flush = true,
+    //     boost::log::keywords::format = "[%ThreadID%][%TimeStamp%] %Message%"
+    // );
 
-    auto torr = Torrent::read_torrent("/home/ferdinand/dev/Fractals/examples/shamanking.torrent");
-    auto torr_ptr = std::make_shared<Torrent>(torr);
+    // std::shared_ptr<std::mutex> mu = make_shared<std::mutex>();
+    // std::unique_lock<std::mutex> lock(*mu.get());
 
-    boost::asio::io_context io;
-    auto bt = BitTorrent(torr_ptr,io);
+    // auto torr = Torrent::read_torrent("/home/ferdinand/dev/Fractals/examples/shamanking.torrent");
+    // auto torr_ptr = std::make_shared<Torrent>(torr);
 
-    try {
-        bt.run();
-    }
-    catch(std::exception e) {
-        std::cout << e.what() << std::endl;
-    }
+    // boost::asio::io_context io;
+    // auto bt = BitTorrent(torr_ptr,io);
+
+    // try {
+    //     bt.run();
+    // }
+    // catch(std::exception e) {
+    //     std::cout << e.what() << std::endl;
+    // }
 }
