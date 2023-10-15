0660a0161fb2a867d866c3ec7f32c631fb1916a8
ferdinand <ferdinandvanwalree@gmail.com>
2023-10-15 18:04:08 +0200

    Fix testTrackerRequest

--- test/network/http/testTrackerRequest.cpp
+++ test/network/http/testTrackerRequest.cpp
@@ -1,13 +1,16 @@
 #include "Path.hpp"
 
+#include "fractals/app/Client.h"
+#include "fractals/common/Tagged.h"
 #include "fractals/common/encode.h"
 #include "fractals/common/utils.h"
 #include "fractals/network/http/Request.h"
 #include "fractals/torrent/Bencode.h"
 #include "fractals/torrent/TorrentMeta.h"
-#include <bencode/decode.h>
 #include <fractals/network/http/TrackerClient.h>
 #include <fractals/torrent/MetaInfo.h>
+
+#include <bencode/decode.h>
 
 #include <fstream>
 #include <gmock/gmock.h>
@@ -21,64 +24,70 @@
 namespace fractals::network::http
 {
 
-torrent::MetaInfo readTorrentFile()
+class TrackerRequestTest : public ::testing::Test
 {
-    std::fstream fs(test::TestData::makePath("dummy_torrent/MyTorrent.torrent"), std::ios::binary | std::ios::in);
-
-    if (!fs.is_open())
+  public:
+    torrent::MetaInfo readTorrentFile()
     {
-        throw std::invalid_argument("Invalid file path");
+        std::fstream fs(test::TestData::makePath("dummy_torrent/MyTorrent.torrent"),
+                        std::ios::binary | std::ios::in);
+
+        if (!fs.is_open())
+        {
+            throw std::invalid_argument("Invalid file path");
+        }
+
+        std::stringstream buffer;
+        buffer << fs.rdbuf();
+
+        const auto mi =
+            torrent::from_bdata<torrent::MetaInfo>(bencode::decode<bdata>(buffer).value());
+
+        fs.close();
+
+        return mi.rightValue;
     }
+};
 
-    std::stringstream buffer;
-    buffer << fs.rdbuf();
-
-    const auto mi = torrent::from_bdata<torrent::MetaInfo>(bencode::decode<bdata>(buffer).value());
-
-    fs.close();
-
-    return mi.rightValue;
-}
-
-TEST(TRACKER_REQUEST, make_tracker_request_empty)
+TEST_F(TrackerRequestTest, make_tracker_request_empty)
 {
     const torrent::SingleFile sf{.name = "", .length = 0, .md5sum = {}};
     const torrent::InfoDict info{
         .piece_length = 0, .pieces = std::vector<char>(), .publish = std::nullopt, .file_mode = sf};
-    const torrent::MetaInfo mi{.announce = "",
+    const torrent::MetaInfo mi{.announce = "announce.url",
                                .announce_list = {},
                                .creation_date = std::nullopt,
                                .comment = std::nullopt,
                                .encoding = std::nullopt,
                                .info = info};
 
-    const TrackerRequest req(mi);
-    // "d6:lengthi0e6:md5sum0:4:name0:12:piecelengthi0e6:pieces0:e"
+    const TrackerRequest req("announce.url", mi, app::generate_peerId());
 
-    std::string bencoded("d6:lengthi0e6:md5sum0:4:name0:12:piece lengthi0e6:pieces0:e");
-    std::cout << common::bytes_to_hex(common::sha1_encode(bencoded)) << std::endl;
-    auto info_hash_hex = "c2fac0dddc699dd42cdbd29d9fdc0a667e0c8402";
-    auto info_hash_bytes = common::hex_to_bytes(info_hash_hex);
-    const TrackerRequest tr("", info_hash_bytes, common::url_encode(info_hash_bytes), req.peer_id,
-                            common::url_encode(req.peer_id), 6882, 0, 0, 0, 0);
+    std::string bencoded("d6:lengthi0e4:name0:12:piece lengthi0e6:pieces0:e");
+    const auto sha1 = common::sha1_encode<20>(bencoded);
+    std::string_view vw{sha1.begin(), sha1.end()};
+    common::InfoHash infoHash{sha1};
+    const TrackerRequest tr("announce.url", infoHash, common::url_encode<20>(infoHash.underlying),
+                            req.peer_id, common::url_encode<20>(req.peer_id), 6882, 0, 0, 0, 0);
 
     ASSERT_EQ(tr, req);
 }
 
-TEST(TRACKER_REQUEST, match_torrent_file)
+TEST_F(TrackerRequestTest, match_torrent_file)
 {
+    const auto APPID = app::generate_peerId();
     const auto sourceMetaInfo = readTorrentFile();
-    const TrackerRequest sourceRequest(sourceMetaInfo); 
+    const TrackerRequest sourceRequest("https://nyaa.si/", sourceMetaInfo, APPID);
 
     const torrent::FileInfo f1{.length = 60, .md5sum = {}, .path = {"dir1", "dir2", "test2 3.txt"}};
     const torrent::FileInfo f2{.length = 35, .md5sum = {}, .path = {"dir1", "dir2", "test2.txt"}};
     const torrent::FileInfo f3{.length = 12, .md5sum = {}, .path = {"dir1", "test2.txt"}};
     const torrent::FileInfo f4{.length = 4, .md5sum = {}, .path = {"test1.txt"}};
-    
+
     const torrent::MultiFile mf{.name = "DummyTorrent"s, .files = {f1, f2, f3, f4}};
     const torrent::InfoDict info{
         .piece_length = 32768, .pieces = sourceMetaInfo.info.pieces, .publish = 0, .file_mode = mf};
-    const std::vector<std::string> announceList{"https://nyaa.si/","https://opentrackr.org/"};
+    const std::vector<std::string> announceList{"https://nyaa.si/", "https://opentrackr.org/"};
     const torrent::MetaInfo mi{.announce = "https://nyaa.si/",
                                .announce_list = {{announceList}},
                                .creation_date = 1677099753,
@@ -87,7 +96,7 @@ TEST(TRACKER_REQUEST, match_torrent_file)
                                .encoding = {"UTF-8"},
                                .info = info};
 
-    TrackerRequest req(mi);
+    TrackerRequest req("https://nyaa.si/", mi, APPID);
     req.peer_id = sourceRequest.peer_id;
     req.url_peer_id = sourceRequest.url_peer_id;
 
